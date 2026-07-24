7febda4ca032f8ccdc85316cc8ec07bda2825d37
Aniket tayde <39806075+aniket200@users.noreply.github.com>
2026-07-24 20:17:57 +0530

    Refactor test_app.py to include Flask app
    Refactor test_app.py to define Flask app and routes.

--- test_app.py
+++ test_app.py
@@ -1,9 +1,20 @@
-from app import app
-def test_home():
-client = app.test_client()
-response = client.get("/")
-assert response.status_code == 200
-def test_health():
-client = app.test_client()
-response = client.get("/health")
-assert response.status_code == 200
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Hello from CodeBuild + Docker!",
+        "hostname": socket.gethostname()
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
